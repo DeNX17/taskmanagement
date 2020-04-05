31f1d85f0268186af4a09c421ebdbe2ffed47cee
Artem164 <artem.lomaev@bravedevelopers.com>
2020-04-05 22:32:56 +0500

    modify task card

--- client/src/task/ui/task.tsx
+++ client/src/task/ui/task.tsx
@@ -16,11 +16,48 @@ interface Props {
 const Root = styled("div")`
   border: 1px solid grey;
   margin: 0px 20px;
+  padding: 10px;
 `
 
-export const Task = ({ title, description }: Props): ReactElement => (
+const Title = styled("p")`
+  font-size: 24px;
+  font-weight: 600;
+  margin: 0;
+`
+
+const Description = styled("p")`
+  color: grey;
+`
+
+const CreatedAt = styled("p")`
+  text-align: right;  
+`
+
+const Footer = styled("div")`
+  display: flex;
+  justify-content: space-between;
+`
+
+const Labels = styled("div")`
+  display: flex;
+`
+
+const Label = styled("p")`
+  padding: 5px 10px;
+`
+
+export const Task = ({ title, description, created_at, comments, author, labels }: Props): ReactElement => (
   <Root>
-    <p>{title}</p>
-    <p>{description}</p>
+    <Title>Title: {title}</Title>
+    <Description>Description: {description}</Description>
+
+    <Footer>
+      <p>Comments count {comments.length}</p>
+      <CreatedAt>{created_at}</CreatedAt>
+    </Footer>
+
+    <p>Author - {author.username}</p>
+
+    <Labels>{labels.map(({ name }: any) => <Label>{name}</Label>)}</Labels>
   </Root>
 )
