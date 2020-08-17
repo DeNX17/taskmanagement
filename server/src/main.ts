f65fccdc4521507c05eff73fe1fa15fae2883a93
Artem164 <artem.lomaev@bravedevelopers.com>
2020-08-17 17:27:22 +0500

    add cluster nestjs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,14 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import cluster from "cluster"
+import { cpus } from "os"
 
 const port = process.env.PORT || 5000
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.enableCors()
+  app.enableCors()
 
   await app.listen(port);
 }
 
-bootstrap();
+
+if (cluster.isMaster) {
+  for (let i = 0; i < cpus().length - 1; i++) {
+    cluster.fork()
+
+    cluster.on("exit", () => {
+      cluster.fork()
+    })
+  }
+}
+
+if (cluster.isWorker) {
+  bootstrap();
+}
+
